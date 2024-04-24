454dda6633f1c6dc134d9f89003eec02b99adbb1
arturtarcisio <arturtarcisio1@gmail.com>
2024-04-23 21:09:29 -0300

    Criando validacoes

--- src/contabanco/ContaTerminal.java
+++ src/contabanco/ContaTerminal.java
@@ -6,18 +6,48 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Por favor, digite o número da Agência:");
-        String agencia = scanner.nextLine();
+        String agencia;
+        do {
+            System.out.println("Por favor, digite o número da Agência (4 dígitos):");
+            agencia = scanner.nextLine();
+            if (agencia.length() != 4 || !agencia.matches("[0-9]+")) {
+                System.out.println("Por favor, informe uma agência válida com 4 dígitos.");
+            }
+        } while (agencia.length() != 4 || !agencia.matches("[0-9]+"));
 
-        System.out.println("Por favor, digite o número da Conta:");
-        int numeroConta = scanner.nextInt();
-        scanner.nextLine(); // Limpar o buffer do scanner
+        int numeroConta;
+        do {
+            System.out.println("Por favor, digite o número da Conta (6 dígitos):");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Por favor, informe uma conta válida com 6 dígitos.");
+                scanner.next();
+            }
+            numeroConta = scanner.nextInt();
+            if (String.valueOf(numeroConta).length() != 6) {
+                System.out.println("Por favor, informe uma conta válida com 6 dígitos.");
+            }
+        } while (String.valueOf(numeroConta).length() != 6);
 
-        System.out.println("Por favor, digite o nome do Cliente:");
-        String nomeCliente = scanner.nextLine();
+        scanner.nextLine();
 
-        System.out.println("Por favor, digite o saldo:");
-        double saldo = scanner.nextDouble();
+        String nomeCliente;
+        do {
+            System.out.println("Por favor, digite o nome do Cliente:");
+            nomeCliente = scanner.nextLine();
+            if (!nomeCliente.matches("[a-zA-Z ]+")) {
+                System.out.println("Por favor, informe um nome válido contendo apenas letras.");
+            }
+        } while (!nomeCliente.matches("[a-zA-Z ]+"));
+
+        double saldo;
+        do {
+            System.out.println("Por favor, digite o saldo:");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Por favor, informe um saldo válido.");
+                scanner.next();
+            }
+            saldo = scanner.nextDouble();
+        } while (saldo < 0);
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
 
